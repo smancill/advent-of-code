36f55a3e55c68b074f05fca024fc4fec9432d34e
Sebastián Mancilla <smancill@smancill.dev>
2023-12-01 21:28:16 -0300

    2023 day 01

---
+++ 2023/README.md
@@ -0,0 +1,1 @@
+Solutions for [Advent of Code 2023](https://adventofcode.com/2023).
---
+++ 2023/day01.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from collections.abc import Sequence
+import re
+from typing import TextIO
+
+
+def parse_data(f: TextIO) -> list[str]:
+    return f.readlines()
+
+
+def part1(data: Sequence[str]) -> int:
+    def get_number(s: str) -> int:
+        m = re.findall(r"\d", s)
+        assert m
+        a, b = int(m[0]), int(m[-1])
+        return 10 * a + b
+
+    return sum(get_number(s) for s in data)
+
+
+def part2(data: Sequence[str]) -> int:
+    def get_digit(d: str) -> int:
+        match d:
+            case "one": return 1
+            case "two": return 2
+            case "three": return 3
+            case "four": return 4
+            case "five": return 5
+            case "six": return 6
+            case "seven": return 7
+            case "eight": return 8
+            case "nine": return 9
+            case _: return int(d)
+
+    def get_number(s: str) -> int:
+        m = re.findall(r"(?=(one|two|three|four|five|six|seven|eight|nine|\d))", s)
+        assert m
+        a, b = get_digit(m[0]), get_digit(m[-1])
+        return 10 * a + b
+
+    return sum(get_number(s) for s in data)
+
+
+def main() -> None:
+    data = parse_data(open(0))
+
+    print(f"P1: {part1(data)}")
+    print(f"P2: {part2(data)}")
+
+
+if __name__ == "__main__":
+    main()
